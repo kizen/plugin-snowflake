5520fb437bdf7c49408de6cb17b2b9fea5224ea3
smfKizen <scott.filips@kizen.com>
2026-06-09 16:09:00 -0500

    UPDATE kizen.json with Snowflake name and description, version back to 1.0.0

--- src/automationSteps/snowflake/script.py
+++ src/automationSteps/snowflake/script.py
@@ -1,21 +1,35 @@
 import http.client
 import json
+import uuid
 
-BASE_URL = "/external-integrations/proxy/scott_f_dev/snowflake_staging_api"
+# PAT config
+SNOWFLAKE_ACCOUNT = "yhjtexo-kv44817"  # host without .snowflakecomputing.com
+SNOWFLAKE_PAT = "eyJraWQiOiI0MDk5NjA2NDEzNjU2MDcwIiwiYWxnIjoiRVMyNTYifQ.eyJwIjoiMjQ0MzU1NTg4OjYyNTU1MDI5NzY1IiwiaXNzIjoiU0Y6MTAxNiIsImV4cCI6MTc4ODgwMDg1OH0.fc9xJVwZLLmN76D_ewx51O_iWtXAg-o6kCeUSmay5XY2R4W6QGt1FwWw99orvmYG61i335Cr6UEfz8tR4K_ylA"  # Your PAT from ALTER USER ... ADD PROGRAMMATIC ACCESS TOKEN
 
-payload = {
-    "statement": "select seq8(), randstr(1000, random()) from table(generator(rowcount=>600))",
-    "timeout": 1000,
-    "database": "SNOWFLAKE_SAMPLE_DATA",
-    "schema": "TPCDS_SF100TCL",
-    "warehouse": "COMPUTE_WH",
-    "bindings": {},
-    "parameters": {},
-    "role": "PUBLIC",
+conn = http.client.HTTPSConnection(f"{SNOWFLAKE_ACCOUNT}.snowflakecomputing.com")
+payload = json.dumps({
+  "statement": "SELECT * FROM TEST_DB.PUBLIC.TEST_CUSTOMERS",
+  "timeout": 1000,
+  "database": "TEST_DB",
+  "schema": "PUBLIC",
+  "warehouse": "COMPUTE_WH",
+  "bindings": {},
+  "parameters": {},
+  "role": "SYSADMIN"
+})
+headers = {
+  'User-Agent': 'myApplication/1.0',
+  'X-Snowflake-Authorization-Token-Type': 'PROGRAMMATIC_ACCESS_TOKEN',
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Authorization': f'Bearer {SNOWFLAKE_PAT}'
 }
 
-outputs.log(f"{BASE_URL}/api/v2/statements?async=false")
+request_id = str(uuid.uuid4())  # Generate unique requestId each time
+conn.request("POST", f"/api/v2/statements?requestId={request_id}&async=false", payload, headers)
+res = conn.getresponse()
 
-res = kizen.api.post(f"{BASE_URL}/api/v2/statements?async=false", json=payload)
+outputs.log(res)
 
-outputs.log(res.json())
+data = res.read()
+outputs.log(data.decode("utf-8"))
